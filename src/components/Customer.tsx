5f77c149eeb2560c3b30bde45c046c1e1cc5b3d0
Eddy G.U <44531054+THE-EDDY@users.noreply.github.com>
2021-12-13 16:23:55 +0100

    UI change - customers

--- src/components/Customer.tsx
+++ src/components/Customer.tsx
@@ -5,6 +5,7 @@ import { useHistory } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import CustomerAPIService from '../api/CustomerAPIService';
 import LoadingActionsCreator from '../actions/LoadingActionsCreator';
+import Dash_Layout from "../layout/dashboard";
 import {
   Tr,
   Td,
@@ -44,41 +45,42 @@ const Customer: React.FC = () => {
   };
 
   return (
-    <>
-      <Flex justifyContent='space-between' marginBottom='2em'>
-        {type === 'employee' && (
-          <Button onClick={handleNewCustomer}>Add New Customer</Button>
-        )}
-      </Flex>
-      <Box>
+      <Dash_Layout title="Customers">
+
+        <Flex justifyContent='space-between' className="mb-4">
+          {type === 'employee' && (
+              <Button onClick={handleNewCustomer}>Add New Customer</Button>
+          )}
+        </Flex>
+
         {loading ? (
-          <Center height='40vh'>
-            <Spinner color='#120c4b' emptyColor='gray.200' size='xl' />
-          </Center>
+            <Center height='40vh'>
+              <Spinner color='#120c4b' emptyColor='gray.200' size='xl' />
+            </Center>
         ) : customers.length ? (
-          <Grid templateColumns='repeat(3, 1fr)' gap={6}>
-            {customers.map((customer, idx) => (
-              <Box onClick={handleClick(customer._id)}>
-                <CustomerCard {...customer} cardType='employee' key={idx} />
-              </Box>
-            ))}
-          </Grid>
+        customers.map((customer, idx) => (
+            <div className="mb-2 w-100" onClick={handleClick(customer._id)}>
+              <CustomerCard {...customer} cardType='employee' key={idx} />
+            </div>
+        ))
         ) : (
-          <Table>
-            <Tr>
-              <Td colspan={12} className='text-center padding_50'>
-                <h4 className='text-danger font-sm-4 mb-2 font-weight-700'>
-                  No Record Found
-                </h4>
-                <p className='lead font-gray'>
-                  There are no Customers at this moment. Check back again later
-                </p>
-              </Td>
-            </Tr>
-          </Table>
+            <Table>
+              <Tr>
+                <Td colspan={12} className='text-center padding_50'>
+                  <h4 className='text-danger font-sm-4 mb-2 font-weight-700'>
+                    No Record Found
+                  </h4>
+                  <p className='lead font-gray'>
+                    There are no Customers at this moment. Check back again later
+                  </p>
+                </Td>
+              </Tr>
+            </Table>
         )}
-      </Box>
-    </>
+
+
+      </Dash_Layout>
+
   );
 };
 
